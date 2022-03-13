19ee4a033e93ce53c9ce5b57411187518cad9b63
Dorjoo <ddorjoo268@gmail.com>
2022-03-13 19:22:38 +0800

    nextjs set up ts

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [
+    './src/components/**/*.{js, ts, tsx, jsx}',
+    './pages/**/*.{js,jsx,ts,tsx}',
+  ],
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
